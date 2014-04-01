dc9aa7257e08c5538329b4d28e4c95a185a22b5d
jpardobl <jpardo@digitalhigh.es>
2014-04-01 13:27:05 +0200

    ported to 2.1

--- cp.php
+++ cp.php
@@ -25,13 +25,15 @@
     POSSIBILITY OF SUCH DAMAGE.
 
 */
-require 'globals.inc';
+
+
+require('globals.inc');
 require("captiveportal.inc");
 require_once("voucher.inc");
 define("BASE_URL", '/cp.php');
-define("VOUCHER_BIN", '/usr/local/bin/voucher');
+//define("VOUCHER_BIN", '/usr/local/bin/voucher');
 
-
+global $cpzone, $g;
 
 
 define("SECRET_KEY", 'michorizo');
@@ -41,7 +43,10 @@ class RollNotFoundException extends Exception{}
 class CantWritePrivateKeyException extends Exception{}
 
 
+
+
 function auth() {
+	//file_put_contents('/tmp/trace', $_SERVER);
     if(
         $_SERVER['auth'] == SECRET_KEY ||
         $_SERVER['Auth'] == SECRET_KEY ||
@@ -63,6 +68,9 @@ if(!auth()){
  * @param int $number The roll number
  */
 function get_rol_index_by_number($db, $number){
+	//print "Get rol index by number:";
+	//print_r($db);
+	//print "Number: ".$number;
     foreach($db as $key => $rol)
         if($rol["number"] == $number)return $key;
     throw new RollNotFoundException("Number:".$number);
@@ -74,9 +82,9 @@ function get_rol_index_by_number($db, $number){
  * @param integer $count The number of vouchers to return
  */
 function retrieve_vouchers($roll_number, $count){
-    global $g, $config;
+    global $g, $config, $cpzone;
 
-    $privkey = base64_decode($config['voucher']['privatekey']);
+    $privkey = base64_decode($config['voucher'][$cpzone]['privatekey']);
     if (!strstr($privkey,"BEGIN RSA PRIVATE KEY")) throw new CantWritePrivateKeyException("There is no RSA key info");
 
     $fd = fopen("{$g['varetc_path']}/voucher.private","w");
@@ -145,19 +153,19 @@ function to_json($roll){
  * Validates the user input for creating new posts
  */
 function valid_post(){
-    global $a_roll, $config;
+    global $a_roll, $config, $cpzone;
+
     // Look for duplicate roll #
     foreach($a_roll as $re) {
             if($re['number'] == $_POST['number']) {
 
                     header("Resource exists", fall, 409);
-                    print sprintf(gettext("Roll number %s already exists."), $_POST['number']);
+                    print "Roll number %s already exists." % $_POST['number'];
                     return false;
             }
     }
-
-    $maxnumber = (1<<$config['voucher']['rollbits']) -1;    // Highest Roll#
-    $maxcount = (1<<$config['voucher']['ticketbits']) -1;   // Highest Ticket#
+    $maxnumber = (1<<$config['voucher'][$cpzone]['rollbits']) -1;    // Highest Roll#
+    $maxcount = (1<<$config['voucher'][$cpzone]['ticketbits']) -1;   // Highest Ticket#
 
     if(!isset($_POST['number']))
         $input_errors[] = "Roll number is missing";
@@ -179,7 +187,8 @@ function valid_post(){
     if($input_errors){
         header("Errors found", fall, 400);
         foreach($input_errors as $error){
-            print $error."<br>";
+
+	    $out .= $error."\n";
         }
         return false;
     }
@@ -190,19 +199,18 @@ function valid_post(){
  * Checks if the config is correctly initialized
  */
 function load_cfg(){
-        global $config;
+        global $config, $cpzone;
+        //print_r( $config["voucher"]);
         if (!is_array($config['voucher'])) {
             print "No existe config de voucher";
             $config['voucher'] = array();
         }
 
-        if (!is_array($config['voucher']['roll'])) {
+        if (!is_array($config['voucher'][$cpzone]['roll'])) {
             print "no existe info de roles";
-            $config['voucher']['roll'] = array();
+            $config['voucher'][$cpzone]['roll'] = array();
         }
-        $a_roll = &$config['voucher']['roll'];
-        #print "la info es ";
-        #print_r($a_roll);
+        $a_roll = &$config['voucher'][$cpzone]['roll'];
         return $a_roll;
 
 }
@@ -212,14 +220,15 @@ function load_cfg(){
 switch($_SERVER['REQUEST_METHOD']){
     case GET:
 
-        if(preg_match('/^\/roll\/(\d+)\/?$/', $_SERVER['PATH_INFO'], $matches))
-            $number = $matches[1];
-        else{
+        if(preg_match('/^\/roll\/(\w+)\/(\d+)\/?$/', $_SERVER['PATH_INFO'], $matches)){
+            $cpzone = $matches[1];
+            $number = $matches[2];
+            
+        }else{
             header("Bad URI", false, 400);
+            print "Bad URI";
             return;
         }
-
-
 
         $a_roll = load_cfg();
         #print_r($a_roll);
@@ -227,6 +236,7 @@ switch($_SERVER['REQUEST_METHOD']){
             $id = get_rol_index_by_number($a_roll, $number);
         }catch(RollNotFoundException $ex){
             header("Not found", false, 404);
+            print "Not found";
             return;
         }
         $rol = $a_roll[$id];
@@ -241,75 +251,78 @@ switch($_SERVER['REQUEST_METHOD']){
     case DELETE:
         $id = -1;
 
-        if(preg_match('/^\/roll\/(\d+)\/?$/', $_SERVER['PATH_INFO'], $matches))
-            $number = $matches[1];
-        else{
+        if(preg_match('/^\/roll\/(\w+)\/(\d+)\/?$/', $_SERVER['PATH_INFO'], $matches)){
+            $cpzone = $matches[1];
+            $number = $matches[2];            
+        }else{
             header("Bad URI", false, 400);
+            print "Bad URI";
             return;
         }
 
-
-        load_cfg();
-        $a_roll = &$config['voucher']['roll'];
+        $a_roll = load_cfg();
         #END load_cfg
 
         try{
             $id = get_rol_index_by_number($a_roll, $number);
         }catch(RollNotFoundException $ex){
             header("Not found", false, 404);
+            print "Not found";
             return;
         }
-
+        
+        
         $roll = $a_roll[$id]['number'];
-        $voucherlck = lock('voucher');
-        unset($a_roll[$id]);
+        $voucherlck = lock("voucher{$cpzone}", LOCK_EX);
+        unset($config['voucher'][$cpzone]['roll'][$id]);
         voucher_unlink_db($roll);
         unlock($voucherlck);
         write_config();
         header("Roll $id deleted", false, 204);
 
-
-
         break;
     case POST:
   #      print "POST";
       //  print $_SERVER['PATH_INFO'];
-        if(!preg_match('/^\/roll\/?$/', $_SERVER['PATH_INFO'], $matches)){
+        if(preg_match('/^\/roll\/(\w+)\/?$/', $_SERVER['PATH_INFO'], $matches)){
+            $cpzone = $matches[1];
+        }else{
             header("Not found", false, 404);
+            print "Not found";
             return;
         }
 
-
-
-        load_cfg();
-        $a_roll = &$config['voucher']['roll'];
-
+        $a_roll = load_cfg();
         if(!valid_post())return;
-
-
+        
+       
+        
+        $voucherlck = lock("voucher{$cpzone}");
+		
+        $rollent['zone']  = $cpzone;
         $rollent['number']  = $_POST['number'];
         $rollent['minutes'] = $_POST['minutes'];
         $rollent['comment'] = $_POST['comment'];
-
-        $voucherlck = lock('voucher');
+        
         $rollent['count'] = $_POST['count'];
-
         $len = ($rollent['count']>>3) + 1;   // count / 8 +1
         $rollent['used'] = base64_encode(str_repeat("\000",$len)); // 4 bitmask
         $rollent['active'] = array();
         voucher_write_used_db($rollent['number'], $rollent['used']);
         voucher_write_active_db($rollent['number'], array());   // create empty DB
         voucher_log(LOG_INFO,sprintf(gettext('All %1$s vouchers from Roll %2$s marked unused'), $rollent['count'], $rollent['number']));
+            
+                         
         unlock($voucherlck);
-        $a_roll[] = $rollent;
-
+        
+        $config['voucher'][$cpzone]['roll'][] = $rollent; 
+        //print_r($a_roll);
+        //print($g['conf_path']);   
         write_config();
 
-        header("Location: ".BASE_URL."/roll/".$_POST["number"], false, 303);
-        print BASE_URL."/roll/".$_POST["number"];
+        header("Location: ".BASE_URL."/roll/".$cpzone."/".$_POST["number"], false, 303);
+        print BASE_URL."/roll/".$cpzone."/".$_POST["number"];
         break;
-
-
 
 }
 
